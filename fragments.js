aaa3d843ddbebbf46f76a29bd02ffa5df0cb0852
Venkata Maheswar Reddy Guntaka <167259287+MrReddy22@users.noreply.github.com>
2025-04-10 07:22:17 +0530

    Create fragments.js

---
+++ fragments.js
@@ -0,0 +1,10 @@
+// These would typically be in an obfuscated or minified script, but we’ll leave it readable-ish for now:
+
+localStorage.setItem('p1', 'Q1RGe3');
+localStorage.setItem('p2', 'Mb2JzY');
+localStorage.setItem('p3', 'WxhZ19');
+localStorage.setItem('p4', 'pX1Nl');
+localStorage.setItem('p5', 'Y3VyZ');
+localStorage.setItem('p6', 'XNfU3');
+localStorage.setItem('p7', 'BlY2V');
+localStorage.setItem('p8', 'yZQ==');
